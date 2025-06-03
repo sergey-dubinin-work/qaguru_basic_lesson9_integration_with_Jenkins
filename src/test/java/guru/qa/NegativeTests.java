39995a7aacf579f23d52d58290ab710b7021641d
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-03 17:41:38 +0300

    add simple tests

---
+++ src/test/java/guru/qa/NegativeTests.java
@@ -0,0 +1,29 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+
+    @Test
+    void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test01() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test02() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test03() {
+        assertTrue(false);
+    }
+
+}
---
+++ src/test/java/guru/qa/SkippedTests.java
@@ -0,0 +1,22 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+
+    @Test
+    @Disabled("With any reason")
+    void test00() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled("With any reason")
+    void test01() {
+        assertTrue(true);
+    }
+
+}
