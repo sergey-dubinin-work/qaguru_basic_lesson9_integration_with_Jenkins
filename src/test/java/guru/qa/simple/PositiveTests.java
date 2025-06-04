04402f6e04a4be70993dd915c8396d31c600fee2
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-04 11:05:00 +0300

    mv tests

--- src/test/java/guru/qa/simple/PositiveTests.java
+++ src/test/java/guru/qa/simple/PositiveTests.java
@@ -1,4 +1,4 @@
-package guru.qa;
+package guru.qa.simple;
 
 import org.junit.jupiter.api.Test;
 
--- src/test/java/guru/qa/simple/SkippedTests.java
+++ src/test/java/guru/qa/simple/SkippedTests.java
@@ -1,4 +1,4 @@
-package guru.qa;
+package guru.qa.simple;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
