15203641e82148ce0080bb3c9d9af15d68a9dde4
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-04 11:56:41 +0300

    add enableVNC and enableVideo

--- src/test/java/guru/qa/demoqa/TestBase.java
+++ src/test/java/guru/qa/demoqa/TestBase.java
@@ -4,6 +4,9 @@ import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
 
 public class TestBase {
 
@@ -12,6 +15,20 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "http://localhost:4444/wd/hub";
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {
+                    {
+                        put("enableVNC", true);
+                        put("enableVideo", true);
+                    }
+                }
+        );
+
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+
     }
 
     @BeforeEach
